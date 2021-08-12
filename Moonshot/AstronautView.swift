d05a966fb11a245a54aa2bad1185045aa74ef4f7
Milo Wyner <milowyner@gmail.com>
2021-08-12 11:49:58 -0700

    Complete challenge #2: in the astronaut view, show all missions they went on

--- Moonshot/AstronautView.swift
+++ Moonshot/AstronautView.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 struct AstronautView: View {
     let astronaut: Astronaut
+    let missions: [Mission]
+    
+    init(astronaut: Astronaut) {
+        self.astronaut = astronaut
+        
+        let allMissions: [Mission] = Bundle.main.decode("missions")
+        
+        self.missions = allMissions.filter { mission in
+            mission.crew.contains { crewRole in
+                crewRole.name == astronaut.id
+            }
+        }
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -22,6 +35,25 @@ struct AstronautView: View {
                     Text(astronaut.description)
                         .padding()
                         .layoutPriority(1)
+                    
+                    ForEach(missions) { mission in
+                        HStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 44, height: 44)
+                            
+                            VStack(alignment: .leading) {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                Text(mission.crew.first {
+                                    $0.name == astronaut.id
+                                }!.role)
+                            }
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
                 }
             }
             .navigationBarTitle(astronaut.name, displayMode: .inline)
